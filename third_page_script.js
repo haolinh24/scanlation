32f3da8c12d8b858a02b84a5999e62df8b4044b0
haolinh24 <57665294+haolinh24@users.noreply.github.com>
2026-09-04 21:09:04 +0700

    Update third_page_script.js new link to data

--- third_page_script.js
+++ third_page_script.js
@@ -1,6 +1,6 @@
 var parseTime = d3.timeParse("%Y");
 //format the data
-d3.csv("https://cdn.glitch.com/e1d93241-ead1-4ad9-ae05-0be9be9a2376%2Fnew_group_chapter_each_year.csv?v=1603182275222", function(error, data) {
+d3.csv("data\new_group_chapter_each_year%20(article%203).csv", function(error, data) {
 data.forEach(function(d) {
   d.date = parseTime(d.Year);
   d.Chapters = +d.Chapters;
